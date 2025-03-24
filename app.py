551fda0fbf4bf73bd7e219ff59ff7705d3defc9b
Tatiana Glinskaya <glintatiana@gmail.com>
2025-03-24 18:01:25 +0300

    Practice part 2 not complete

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 import random
+
+
 
 app = Flask(__name__)
 app.json.ensure_ascii = False
 
 @app.route("/")
-def hello_world(): # ффункция-обработчик, вызванная при запросе URL корневого 
+def hello_world():
+    """
+    Обработчик для корневого URL 
+    """
     return "Hello, World!"
 
 about_me = {
@@ -16,6 +21,9 @@ about_me = {
 
 @app.route("/about")
 def about():
+    """
+    Информация по URL /about
+    """
     return about_me
 
 quotes = [
@@ -46,19 +54,42 @@ quotes = [
     },
 ]
 
+field_dict = ['id', 'author', 'text']
+
 @app.route("/quotes")
 def my_quotes():
+    """
+    Возвращает все цитаты по URL /quotes
+    """
     return quotes
 
-@app.route("/quotes/<int:quote_id>")
-def show_quote(quote_id):
+def get_quote_by_id(quote_id):
+    """
+    Метод для получения цитаты по ID - возвращаем объект целиком (Практика часть 2 пункт 3)
+    """
     for i in quotes:
         if i['id'] == quote_id:
             return i
-    return f"Quote with id={quote_id} not found", 404
+    return {}
+
+@app.route("/quotes/<int:quote_id>")
+def show_quote(quote_id):
+    """
+    Метод для динамического задания номера цитаты через URL /quites/номер_цитаты
+    Если цитаты нет - возвращаем 404 
+    """
+    ans = get_quote_by_id(quote_id)
+    if ans == {}:
+        return f"Quote with id={quote_id} not found", 404
+    else:
+        return ans
 
 @app.route("/quotes/<path:subpath>")
 def show_quote_count(subpath):
+    """
+    Метод для тестирования указания динамического URL 
+    Обрабатывает /count 
+    """
     if subpath == 'count':
         cnt_obj = {"count": len(quotes)}
         return cnt_obj
@@ -66,7 +97,40 @@ def show_quote_count(subpath):
 
 @app.route("/quotes/rand")
 def show_rand_quote():
+    """
+    Возвращаем рандомную цитату по URL /quotes/rand
+    """
     return random.choice(quotes)
+
+def fn_get_new_quote_id():
+    return quotes[-1]['id']+1
+
+@app.route("/quotes", methods=['POST'])
+def create_quote():
+    """
+    метод для создания новой цитаты в справочнике цитат через POST (Практика часть 2 пункт 1)
+    реализована функция fn_get_new_quote_id для генерации нового id под новую цитату
+
+    возвращается создаваемый объект (Практика часть 2 пункт 2)
+    """
+    data = request.json
+    data['id'] = fn_get_new_quote_id()
+    quotes.append(data)
+    return data, 201
+
+@app.route("/quotes/<int:quote_id>", methods=['PUT'])
+def edit_quote(id):
+    new_data = request.json
+    quote = get_quote_by_id(new_data['id'])
+    if quote == {}:
+        return f"Quote with id={new_data['id']} not found", 404
+    else:
+        for j in new_data: # итерируем по всем возможным полям
+            if j not in field_dict:
+               return f"Quote key "{j}" not found", 404 
+             
+        return quote, 200
+
 
 if __name__ == "__main__":
     app.run(debug=True) 
