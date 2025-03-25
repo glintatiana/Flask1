5a5119ed9e1d50e61a3659e7dd9588812148cd8f
Tatiana Glinskaya <glintatiana@gmail.com>
2025-03-25 16:34:55 +0300

    Added sqlite

---
+++ sqlite_example/sql_create_table.py
@@ -0,0 +1,19 @@
+import sqlite3
+create_table = """
+CREATE TABLE if not exists quotes(id INTEGER PRIMARY KEY AUTOINCREMENT,
+author TEXT NOT NULL,
+text TEXT NOT NULL
+);
+"""
+# Подключение в БД
+connection = sqlite3.connect("store.db")
+# Создаем cursor, он позволяет делать SQL-запросы
+cursor = connection.cursor()
+# Выполняем запрос:
+cursor.execute(create_table)
+# Фиксируем выполнение(транзакцию)
+connection.commit()
+# Закрыть курсор:
+cursor.close()
+# Закрыть соединение:
+connection.close()
