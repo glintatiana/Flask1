81397d426a18493c95ca0f35e83f86017bc99231
Tatiana Glinskaya <glintatiana@gmail.com>
2025-03-26 14:59:34 +0300

    Adding some sqlalchemy

--- app.py
+++ app.py
@@ -1,55 +1,62 @@
 from flask import Flask, request, g
-import random
 from pathlib import Path
+
+#sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+import random
 import sqlite3
 
-app = Flask(__name__)
-app.json.ensure_ascii = False
+class Base(DeclarativeBase):
+    pass
+
 
 BASE_DIR = Path(__file__).parent
 path_to_db = BASE_DIR / "sqlite_example/store.db" # <- тут путь к БД
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(path_to_db)
-    return db
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR / 'main.db'}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class QuoteModel(db.Model):
+    __tablename__ = 'quotes'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    author: Mapped[str] = mapped_column(String(32))
+    text: Mapped[str] = mapped_column(String(255))
+    rating : Mapped[int] = mapped_column(default = 1)
+
+    def __init__(self, author, text, rating = 1):
+        self.author = author
+        self.text = text
+
+    def to_dict(self):
+        return {
+            "id" : self.id,
+            "author" : self.author,
+            "text" : self.text,
+            "rating" : self.rating
+        }
 
 
 field_dict = ['id', 'author', 'text', 'rating']
 
-def tuple_to_dict(keys, tuple_list):
-    """
-    Метод для преобразования списка кортежей в список словарей
-    На вход - ключи + список кортежей 
-    """
-    dict_list = []
-    for dict_ in tuple_list:
-        dict_l = dict(zip(keys, dict_))
-        dict_list.append(dict_l)
-    
-    return dict_list
 
 def get_quote_by_id(quote_id):
     """
     Метод для получения цитаты по ID - возвращаем объект целиком
     """
+    quote = db.one_or_404(db.select(QuoteModel).filter_by(id=quote_id))
 
-    cursor = get_db().cursor()
-    quote = cursor.execute("SELECT * from quotes where id = ?", (quote_id,)).fetchone()
-    
-    # для обработчиков на пустое 
-    if quote:
-        keys = ("id", "author", "text", "rating")
-        quote_dict = tuple_to_dict(keys, (quote,)) # преобразуем в tuple, так как возвращается только одна строка 
-        return quote_dict
-    return {}
+    return quote.to_dict(), 200
 
 @app.route("/quotes")
 def my_quotes():
@@ -57,14 +64,8 @@ def my_quotes():
     Метод возвращает список всех цитат
     Чтение идет из бд sqlite 
     """
-    cursor = get_db().cursor()
-    cursor.execute("SELECT * from quotes")
-    quotes_db = cursor.fetchall()
-
-    # подготовка данных - нужен список словарей
-    # нужно преобразовать список кортежей в список словарей
-    keys = ("id", "author", "text", "rating")
-    quotes = tuple_to_dict(keys, quotes_db)
+    quotes_db = db.session.scalars(db.select(QuoteModel)).all()
+    quotes = [q.to_dict() for q in quotes_db]
 
     return quotes, 200
 
@@ -87,11 +88,9 @@ def show_quote_count(subpath):
     Обрабатывает /count 
     """
     if subpath == 'count':
-        cursor = get_db().cursor()
-        cursor.execute("SELECT count(1) from quotes")
-        quotes_cnt = cursor.fetchone() #get list[tuple]
+        quotes_db = db.session.scalars(db.select(QuoteModel)).all()
 
-        return  {"count": quotes_cnt[0]}, 200
+        return  {"count": len(quotes_db)}, 200
     return f"Page not found", 503
 
 @app.route("/quotes", methods=['POST'])
@@ -102,29 +101,16 @@ def create_quote():
     """
     data = request.json
 
-    # проверяем, все ли ключи - валидные
-    for key in data:
-        if key not in field_dict:
-            return f"Quote key '{key}' is not valid", 400 
-
-    connection = get_db()
-    cursor = connection.cursor()
-
     rating_norm = data.get('rating')
     if rating_norm is None or rating_norm not in range(1,6):
         rating_norm = 1
-    cursor.execute("insert into quotes (author, text, rating) values (?,?,?)",(data.get('author'), data.get('text'), rating_norm))
-    rowid = cursor.lastrowid
 
-    connection.commit()
-  
-    cursor.execute("SELECT * from quotes where rowid = ?", (rowid,))
-    quote = cursor.fetchall() #get list[tuple]
+    quote = QuoteModel(data.get('author'), data.get('text'), rating_norm)
+    db.session.add(quote)
+    db.session.commit()
 
-    keys = ("id", "author", "text", "rating")
-    quote_list = tuple_to_dict(keys, quote)
+    return db.session.get(QuoteModel, quote.id).to_dict(), 201
 
-    return quote_list, 201
 
 @app.route("/quotes/<int:quote_id>", methods=['PUT'])
 def edit_quote(quote_id):
@@ -133,42 +119,41 @@ def edit_quote(quote_id):
     """
     new_data = request.json
 
-    connection = get_db()
-    cursor = connection.cursor()
-
     rating_norm = new_data.get('rating')
     if rating_norm is None or rating_norm not in range(1,6):
         rating_norm = 1
-    new_quote_data = (new_data.get('author'), new_data.get('text'), rating_norm)    
-    update_quote = "UPDATE quotes SET author=coalesce(?, author), text=coalesce(?, text), rating=coalesce(?, rating) WHERE id=?"
-    cursor.execute(update_quote, (*new_quote_data, quote_id))
-    rowcnt = cursor.rowcount
 
-    connection.commit()
+    quote = db.session.get(QuoteModel, quote_id)
+    if not quote:
+         return f"Quote with id={quote_id} not found", 404
 
-    if rowcnt == 0:
-        return f"Quote with id={quote_id} not found", 404
+    if quote and not (set(new_data.keys()) - set (('author','text', 'rating'))):
+        if 'author' in new_data.keys():
+            quote.author = new_data.get('author')
+        if 'text' in new_data.keys():
+            quote.text = new_data.get('text')
+        if 'rating' in new_data.keys():
+            quote.rating = rating_norm
 
-    return get_quote_by_id(quote_id), 200
+    db.session.commit()
+
+    return quote.to_dict(), 200 
+
 
 @app.route("/quotes/<int:quote_id>", methods=['DELETE'])
 def delete(quote_id):
     """
     Метод для удаления цитаты из списка по её ID через DELETE 
     """
-    connection = get_db()
-    cursor = connection.cursor()
-    
-    delete_quote = "delete from quotes WHERE id=?"
-    cursor.execute(delete_quote, (quote_id,))
-    rowcnt = cursor.rowcount
 
-    connection.commit()
+    quote = db.session.get(QuoteModel, quote_id)
+    if not quote:
+         return f"Quote with id={quote_id} not found", 404
 
-    if rowcnt == 0:
-        return f"Quote with id={quote_id} not found", 404
+    db.session.delete(quote)
+    db.session.commit()
 
     return f"Quote with id {quote_id} is deleted.", 200
-
+    
 if __name__ == "__main__":
     app.run(debug=True) 
