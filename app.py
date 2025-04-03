0fc0ec03c8ac1b45e05542b12f2bc663d4461462
Tatiana Glinskaya <glintatiana@gmail.com>
2025-04-03 17:08:19 +0300

    adding rating to db

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ class QuoteModel(db.Model):
     author_id: Mapped[str] = mapped_column(ForeignKey('authors.id'))
     author: Mapped['AuthorModel'] = relationship(back_populates='quotes')
     text: Mapped[str] = mapped_column(String(255))
-    # rating: Mapped[int] = mapped_column(Integer, default = 1, nullable = True)
+    rating: Mapped[int] = mapped_column(nullable = False, server_default = '1')
 
     def __init__(self, author, text):
         self.author = author
